f2eff33b0d30b252d4e2c6cdf0784bea00790b87
Patrick Navarre <patrick.navarre@codeimmersives.com>
2021-03-22 10:13:22 -0400

    App.js Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
+import jwtDecode from 'jwt-decode';
 import React, { Component } from 'react'
+import { ToastContainer } from 'react-toastify';
 import MainRouter from './MainRouter'
 
 export class App extends Component {
+  state = {
+    user: null,
+  }
+
+  componentDidMount() {
+
+    let getJwtToken = localStorage.getItem("jwtToken")
+
+    console.log(getJwtToken);
+    if (getJwtToken) {
+      const currentTime = Date.now() / 1000;
+
+      console.log(currentTime);
+
+      let decodedJwtToken = jwtDecode(getJwtToken)
+      console.log(decodedJwtToken);
+
+      if (decodedJwtToken.exp < currentTime) {
+        this.handleUserLogout();
+      } else {
+        this.handleUserLogin(decodedJwtToken);
+      }
+    }
+  }
+
+  handleUserLogin = (user) => {
+    console.log(32, "from app");
+    console.log(user);
+    this.setState({
+      user: {
+        email: user.email,
+      }
+    })
+  };
+
+  handleUserLogout = () => {
+    localStorage.removeItem("jwtToken")
+    this.setState({
+      user: null,
+    })
+  }
+
   render() {
     return (
-      <div>
-       <MainRouter />
-      </div>
+      <>
+       <ToastContainer />
+       <MainRouter 
+         user={this.state.user}
+         handleUserLogin={this.handleUserLogin}
+         handleUserLogout={this.handleUserLogout}
+       />
+      </>
     )
   }
 }
